5eff8ad953d2b6c57476eed85e0cedb0de1d92cf
BrockH1 <hoskinsbe@gmail.com>
2023-05-14 19:42:50 -0600

    added favicon

--- Chamber/scripts/getDate.js
+++ Chamber/scripts/getDate.js
@@ -1,3 +1,11 @@
 document.getElementById('year').innerHTML = new Date().getFullYear();
 
 document.getElementById('lastModified').innerHTML = document.lastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
